faa2a54f6c6ab5794d4236f6d0d6cbe6967055e8
tyron <wishsmp1002@gmail.com>
2026-03-06 16:03:55 +1100

    Spigot Command Arguments

--- src/main/java/net/saturn/pluginCourse/TestCommand.java
+++ src/main/java/net/saturn/pluginCourse/TestCommand.java
@@ -1,9 +1,12 @@
 package net.saturn.pluginCourse;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Warden;
 
 public class TestCommand implements CommandExecutor {
     @Override
@@ -14,6 +17,9 @@ public class TestCommand implements CommandExecutor {
             if (args.length == 1) {
                 if (args[0].equalsIgnoreCase("hello")) {
                     ((Player) sender).sendMessage("Hello Idiot Head");
+                } else if (args[0].equalsIgnoreCase("meow")) {
+                    ((Player) sender).sendMessage(ChatColor.AQUA + "Meow!");
+                    ((Player) sender).setHealth(18);
                 }
             }
 
