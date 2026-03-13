fdcd7203cb70a1b7baa90576b35ebac96c4c8a22
tyron <wishsmp1002@gmail.com>
2026-03-13 18:43:22 +1100

    Entity Code Working (NO AI) Finally I am Smart!!!

--- src/main/java/net/saturn/pluginCourse/PluginCourse.java
+++ src/main/java/net/saturn/pluginCourse/PluginCourse.java
@@ -1,15 +1,7 @@
 package net.saturn.pluginCourse;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.entity.*;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-
-import org.bukkit.event.entity.CreatureSpawnEvent;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.event.entity.EntityEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PluginCourse extends JavaPlugin {
@@ -17,40 +9,13 @@ public final class PluginCourse extends JavaPlugin {
     @Override
     public void onEnable() {
         System.out.println("OUR PLUGIN IS WORKING");
+        PluginManager pluginManager = Bukkit.getPluginManager();
 
-        ArmorStand stand = (ArmorStand) Bukkit.getWorld("world").spawnEntity
-                (new Location(Bukkit.getWorld("world"), 0, 70, 0), EntityType.ARMOR_STAND);
+        pluginManager.registerEvents(new EntityHitEvent(), this);
 
-    }
-
-    @EventHandler
-    public void onEntitySpawn(CreatureSpawnEvent e) {
-
-        EntityType entityType = EntityType.CREEPER;
-
-        if(entityType.isSpawnable()) {
-            e.setCancelled(true);
-        }
-    }
-
-    @EventHandler
-    public void onEntityDeath(EntityDeathEvent e) {
 
     }
 
-    @EventHandler
-    public void onEntityDamage (EntityDamageByEntityEvent e) {
-
-    }
-
-    @EventHandler
-
-    public void entityEvents(EntityEvent e) {
-
-        // If you do EntityEvent you can look at all or
-        // almost all the EntityEvents you can do and look at
-
-    }
 
 
     @Override
